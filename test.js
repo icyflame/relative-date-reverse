8239c818a109e69ee6b57816be0040f5194eaee0
Siddharth Kannan <siddharthkannan@protonmail.com>
2016-06-12 13:24:22 +0530

    Move to mocha, update dev deps versions

--- test.js
+++ test.js
@@ -1,8 +1,37 @@
-/* global it */
-'use strict';
-var assert = require('assert');
-// var relativeDateReverse = require('./');
+import test from 'ava';
+var relative = require('./');
+var dateable = require('dateable');
+var day = 24 * 60 * 60 * 1000;
 
-it('should ', function () {
-  assert.strictEqual(true, true);
+test('unformattable returns false', t => {
+	var returned = relative('some days ago');
+	t.false(returned[0]);
+});
+
+test('special strings works', t => {
+	var returned;
+
+	var returned = relative("yesterday");
+	var expected = new Date(new Date().getTime() - day);
+
+	t.true(returned[0]);
+	t.true(dateable(returned[1], 'YYYY-MM-DD') === dateable(expected, 'YYYY-MM-DD'));
+
+	var returned = relative("day before yesterday");
+	var expected = new Date(new Date().getTime() - 2 * day);
+
+	t.true(returned[0]);
+	t.true(dateable(returned[1], 'YYYY-MM-DD') === dateable(expected, 'YYYY-MM-DD'));
+
+	var returned = relative("tomorrow");
+	var expected = new Date(new Date().getTime() + day);
+
+	t.true(returned[0]);
+	t.true(dateable(returned[1], 'YYYY-MM-DD') === dateable(expected, 'YYYY-MM-DD'));
+
+	var returned = relative("day after tomorrow");
+	var expected = new Date(new Date().getTime() + 2 * day);
+
+	t.true(returned[0]);
+	t.true(dateable(returned[1], 'YYYY-MM-DD') === dateable(expected, 'YYYY-MM-DD'));
 });
