48adc2b4024f7c8dd1e120c030510b0675c493ae
Siddharth Kannan <siddharthkannan@protonmail.com>
2016-06-12 13:24:03 +0530

    Add recognition of some special strings

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 'use strict';
-module.exports = function (str, opts) {
+module.exports = function (str) {
   if (typeof str !== 'string') {
-    throw new TypeError('Expected a string');
+    throw new TypeError('Expected a string!');
   }
 
-  opts = opts || {};
+  var defaultReturn = [false, ''];
 
-  return str + ' & ' + (opts.postfix || 'rainbows');
+  // constants
+  var dayMilliSeconds = 24 * 60 * 60 * 1000;
+
+  // A list of strings along with the offset to the date that they create
+  var specialStrings = {
+    'yesterday': -1 * dayMilliSeconds,
+    'tomorrow': +1 * dayMilliSeconds,
+    'day before yesterday': -2 * dayMilliSeconds,
+    'day after tomorrow': +2 * dayMilliSeconds
+  };
+
+  for (var i in specialStrings) {
+    if (str === i) {
+      return [true, new Date(new Date().getTime() + specialStrings[i])];
+    }
+  }
+
+  // check for the most common regexes
+
+  // var daysAgo = /([0-9]+) (days|weeks) ago/i;
+
+  return defaultReturn;
 };
