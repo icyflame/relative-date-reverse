f5a86834fb781a0b1f9ac4eaac199921656698d8
Siddharth Kannan <siddharthkannan@protonmail.com>
2016-09-04 18:16:11 +0530

    Add new tests to check new regexes

--- test.js
+++ test.js
@@ -35,11 +35,19 @@ test('special strings works', t => {
 	t.true(returned[0]);
 	t.true(dateable(returned[1], 'YYYY-MM-DD') === dateable(expected, 'YYYY-MM-DD'));
 
+});
+
+test('re1 works: n days ago', t => {
+
 	var returned = relative("5 days ago")
 	var expected = new Date(new Date().getTime() - 5 * day);
 
 	t.true(returned[0]);
 	t.true(dateable(returned[1], 'YYYY-MM-DD') === dateable(expected, 'YYYY-MM-DD'));
+
+});
+
+test('re2 works: last <day-name>/<day-abbreviation>', t => {
 
 	var returned = relative("last Tuesday")
 	var expected = new Date(new Date().getTime() - (7 - Math.abs(new Date().getDay() - 2)) * day);
@@ -47,8 +55,36 @@ test('special strings works', t => {
 	t.true(returned[0]);
 	t.true(dateable(returned[1], 'YYYY-MM-DD') === dateable(expected, 'YYYY-MM-DD'));
 
+	var returned = relative("last Tue")
+	var expected = new Date(new Date().getTime() - (7 - Math.abs(new Date().getDay() - 2)) * day);
+
+	t.true(returned[0]);
+	t.true(dateable(returned[1], 'YYYY-MM-DD') === dateable(expected, 'YYYY-MM-DD'));
+
+	var returned = relative("previous Friday")
+	var expected = new Date(new Date().getTime() - (7 - Math.abs(new Date().getDay() - 5)) * day);
+
+	t.true(returned[0]);
+	t.true(dateable(returned[1], 'YYYY-MM-DD') === dateable(expected, 'YYYY-MM-DD'));
+
+});
+
+test('re3 works: absolute date in the past 1 year', t => {
+
 	var returned = relative("7th April")
 	var expected = new Date(new Date(new Date().setMonth(3)).setDate(7));
+
+	t.true(returned[0]);
+	t.true(dateable(returned[1], 'YYYY-MM-DD') === dateable(expected, 'YYYY-MM-DD'));
+
+	var returned = relative("12th December")
+	var expected = new Date(new Date(new Date().setMonth(11)).setDate(12));
+
+	t.true(returned[0]);
+	t.true(dateable(returned[1], 'YYYY-MM-DD') === dateable(expected, 'YYYY-MM-DD'));
+
+	var returned = relative("19th Nov")
+	var expected = new Date(new Date(new Date().setMonth(10)).setDate(19));
 
 	t.true(returned[0]);
 	t.true(dateable(returned[1], 'YYYY-MM-DD') === dateable(expected, 'YYYY-MM-DD'));
